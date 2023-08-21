f0369be7cecccfa80be4cb8d49034a0438592442
Raja <raja.kcpal@gmail.com>
2023-08-21 07:38:19 +0545

    Templating setup backend

--- routes/admin.js
+++ routes/admin.js
@@ -6,9 +6,18 @@ const rootDir = require('../utils/path');
 
 const router = express.Router();
 
+const products = [];
+
 // /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    activeAddProduct: true,
+    formsCSS: true,
+    productCSS: true,
+  });
+    // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 //   res.send(
 //     '<form action ="/admin/add-product" method="POST"><input type="text" name="title"><button>Add Product</button></form>'
 //   );
@@ -16,8 +25,9 @@ router.get("/add-product", (req, res, next) => {
 
 // /admin/add-product => POST
 router.post("/add-product", (req, res, next) => {
-  console.log(req.body);
+  products.push({title: req.body.title});
   res.redirect("/");
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
