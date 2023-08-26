aee356a5ba98730a6264bd73aea2344c2d4c6db4
Raja <raja.kcpal@gmail.com>
2023-08-26 21:18:05 +0545

    Mongodb driver installed and connected successfully to mongodb cloud.

--- utils/database.js
+++ utils/database.js
@@ -1,11 +1,29 @@
-const { Sequelize } = require("sequelize");
+const mongodb = require('mongodb');
 
-const sequelize = new Sequelize("app_db", "app_user", "app_password", {
-  host: "db",
-  dialect: "mysql",
-});
+const MongoClient = mongodb.MongoClient;
 
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://kcp:ruwGqtZ0jUn95j33@cluster0.ybpyzxn.mongodb.net/?retryWrites=true&w=majority"
+  )
+    .then((client) => {
+      console.log("Connected!");
+      callback(client);
+    })
+    .catch((err) => console.log(err));
+}
+
+module.exports = mongoConnect;
+
+
+// const { Sequelize } = require("sequelize");
+
+// const sequelize = new Sequelize("app_db", "app_user", "app_password", {
+//   host: "db",
+//   dialect: "mysql",
+// });
+
+// module.exports = sequelize;
 
 // const mysql = require("mysql2");
 
